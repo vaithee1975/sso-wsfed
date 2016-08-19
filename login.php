05907b2a171078e97d5df45864488dcdf9bb728a
varun_sankar <varun.sankaranarayana@htcindia.com>
2016-08-19 12:41:44 +0530

    ADFS

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 // uncomment this to display internal server errors.
 //error_reporting(E_ALL);
 //ini_set('display_errors', 'On');
-ini_set('include_path', ini_get('include_path').';libraries/;');
+ini_set('include_path', ini_get('include_path').';../libraries/;');
 require_once ('waad-federation/TrustedIssuersRepository.php');
 ?>
 <!DOCTYPE html>
